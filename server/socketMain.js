9713973f22a83ab0fd42053d7a37227a6cc8afc5
Anurag Bajaj <anuragb26@gmail.com>
2020-02-11 08:00:02 +0530

    Feat:Socket client to server connections

--- server/socketMain.js
+++ server/socketMain.js
@@ -1,5 +1,11 @@
 /*
  entrypoint for all  clusters which will make workers 
  and the workers will do the Socket.io handling 
- 
+
  */
+
+function socketMain(io, socket) {
+  console.log("A socket connected", socket.id);
+}
+
+module.exports = socketMain;
