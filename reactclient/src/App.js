3a0c1a91018758140755cc5058e0abcbcde7b78a
Anurag Bajaj <anuragb26@gmail.com>
2020-02-20 06:09:00 +0530

    Fix:Change hooks to class based

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,26 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import socket from "./utilities/socketConnection";
 import Widget from "./Widget";
-import "./App.css";
 
-function App() {
-  const [performanceData, setPerformanceData] = useState({});
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      performanceData: {}
+    };
+  }
 
-  socket.on("data", data => {
-    // inside this callback we got new data
-    // update state to rerender app
-    console.log("performanceData inside", performanceData);
-    const currentPeformanceData = { ...performanceData };
-    console.log("macA", data);
-    currentPeformanceData[data.macA] = data;
-    setPerformanceData(currentPeformanceData);
-  });
-  console.log("performanceData", performanceData);
-  const widgets = [];
-  Object.entries(performanceData).forEach(([key, value]) => {
-    widgets.push(<Widget key={key} data={value} />);
-  });
-  return <div className="App">{widgets}</div>;
+  componentDidMount() {
+    socket.on("data", data => {
+      // inside this callback, we just got some new data!
+      // let's update state so we can
+      // re-render App --> Widget --> CPU/Mem/Info
+      // we need to make a copy of current state
+      // so we can mutate it!
+      const currentState = { ...this.state.performanceData };
+      // const currentState = Object.assign(this.state.performanceData,{})
+      // currentState is an object! Not an array!
+      // the reason for this is so we can use the machine's
+      // MacA as it's property
+      currentState[data.macA] = data;
+      this.setState({
+        performanceData: currentState
+      });
+    });
+  }
+
+  render() {
+    console.log(this.state.performanceData);
+    let widgets = [];
+    const data = this.state.performanceData;
+    // grab each machine, by property, from data
+    Object.entries(data).forEach(([key, value]) => {
+      widgets.push(<Widget key={key} data={value} />);
+    });
+    return <div className="App">{widgets}</div>;
+  }
 }
 
 export default App;
